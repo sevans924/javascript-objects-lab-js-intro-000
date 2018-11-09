13211f4a9e0c220c48cf1879b8a2b1c8fdbd2693
Sarah Elzinga <sevans924@gmail.com>
2018-11-09 19:56:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+var recipes = {};
+
+function updateObjectsWithKeyAndValue(object, key, value) {
+
+  return Object.assign({}, object, { [key]: value })
+}
+
+function updateObjectsWithKeyAndValue()
+
+deleteFromObjectByKey(object, key) {
+
+}
